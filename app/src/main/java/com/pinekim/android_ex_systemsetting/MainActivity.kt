8522d1faeb63c7c2adda3fbc6b456d9f6d7fb529
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2023-05-17 20:34:39 +0900

    Move to WIFI Setting Page

--- app/src/main/java/com/pinekim/android_ex_systemsetting/MainActivity.kt
+++ app/src/main/java/com/pinekim/android_ex_systemsetting/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.pinekim.android_ex_systemsetting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Button
+import com.pinekim.android_ex_systemsetting.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mBinding: ActivityMainBinding? = null
+    private val binding get() = mBinding!!
+
+    private var ButtonWifi: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ButtonWifi = binding.btnSettingWifi
+
+        ButtonWifi?.setOnClickListener {
+            val i = Intent(Settings.ACTION_WIFI_SETTINGS)
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(i)
+        }
     }
 }
