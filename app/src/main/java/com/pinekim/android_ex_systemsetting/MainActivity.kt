d19cf5322eee018e2f25b3ba23c160bb144d3638
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2023-05-17 20:45:27 +0900

    Move to Bluetooth Setting Page

--- app/src/main/java/com/pinekim/android_ex_systemsetting/MainActivity.kt
+++ app/src/main/java/com/pinekim/android_ex_systemsetting/MainActivity.kt
@@ -12,16 +12,25 @@ class MainActivity : AppCompatActivity() {
     private var mBinding: ActivityMainBinding? = null
     private val binding get() = mBinding!!
 
-    private var ButtonWifi: Button? = null
+    private var btnWifi: Button? = null
+    private var btnBluetooth: Button? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        ButtonWifi = binding.btnSettingWifi
+        btnWifi = binding.btnSettingWifi
+        btnBluetooth = binding.btnSettingBluetooth
 
-        ButtonWifi?.setOnClickListener {
+        btnWifi?.setOnClickListener { // 와이파이 연결 이동
+            // ACTION_WIFI_IP_SETTINGS - 와이파이 고급 설정
             val i = Intent(Settings.ACTION_WIFI_SETTINGS)
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(i)
+        }
+
+        btnBluetooth?.setOnClickListener { // 블루투스 연결 이동
+            val i = Intent(Settings.ACTION_BLUETOOTH_SETTINGS)
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(i)
         }
